1494ebac0320ee2a09284d1188453565ace38d0b
John Lawson <johnroblawson@gmail.com>
2018-12-26 23:43:54 +0000

    Post-shutdown PB sync

---
+++ pycode/new_verif.py
@@ -0,0 +1,56 @@
+import os
+import pdb
+
+
+### Domains for each case
+# Import first init time for each case
+# Load the lat/lon .npy files for d01, d02.
+
+### CAPE/shear for each case
+# Top-down graphs of max from SPC mesoanal?
+# Bar graph inset somewhere?
+
+### Performance diagram for point-to-point
+# QPF and cref
+
+
+### CRPS of QPF/cref
+
+
+
+#### eFSS compare 3km/1km
+# Do over a lot of scales to check generality
+# Select about 5 good scales to overlap
+# Need to include 1 km "off the edge" of 3 km eFSS
+# Do 1-, 3-, 5-step for time windows
+
+### eSAL for 1km and 3 km
+
+##### OBJECT ID ######
+### Do object ID for cref (obs and sim) and w (forecast)
+
+
+# Performance diagram of object cref
+
+
+# CRPS of cref? Look at eSAL
+
+
+### Max W of all objects
+
+
+### Waffleplot of matched differences in W, cref...
+# The fill of the square is the difference
+# Colour so it's negative or positive, and the colour depth is how far
+# Order it so it's "predictive power" of 3 v 1 km.
+
+### Case study 
+# 500/925 heights, ~sfc fronts (Python!), auto front detection?!
+# CAPE/shear
+# forecast v observed rotation, cref, precip, W?
+
+
+### SCHEMATIC
+# PMM 'average object' for each domain?
+# Pick the object that has the closest to the average for each domain, for one case?
+
