e4f138fb49e788ee7c8bf7d50aaf177153f51882
John Lawson <john.lawson@noaa.gov>
2019-02-14 15:26:50 -0600

    Started script for case outline figure

---
+++ pycode/figure_caseoutline.py
@@ -0,0 +1,22 @@
+import pdb
+import os
+
+import numpy as N
+
+import evac.utils as utils
+
+
+# Load GFS or NAM data 
+
+# Load tornado report CSV
+
+# Load severe hail reports, or draw a swathe of SVR reports?
+
+
+# For each case...
+
+# Plot shear and CAPE
+
+# Plot 300 hPa winds, 500 hPa height, MSLP, sfc fronts
+
+# Thumbnail of reflectivity with reports
