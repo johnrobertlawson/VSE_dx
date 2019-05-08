b4589f8d5789648ac44535222c119f619c05bd7a
John Lawson <johnroblawson@gmail.com>
2019-05-08 10:13:46 -0500

    Fix for order of logic

--- pycode/new_verif_parallel.py
+++ pycode/new_verif_parallel.py
@@ -894,18 +894,25 @@ def loop_ens_data(fcst_vrbl,fcst_fmts):
         for initutc in initutcs:
             for fcst_fmt in fcst_fmts:
                 if (not fcst_fmt.startswith("d0")):
-                    # OBS
+                    obs = True
+                    fcst = False
                     if fcst_vrbl.startswith("UH"):
                         fcst_vrbl = fcst_vrbl.replace("UH","AWS")
-                    prod_code = "_".join((fcst_fmt, "obs"))
                     member_names = ('obs',)
                     fcmns = (0,)
                 else:
+                    obs = False
+                    fcst = True
                     # FCST
-                    prod_code = "_".join((fcst_fmt, mem))
                     fcmns = all_fcstmins
                 # pdb.set_trace()
                 for mem in member_names:
+                    if obs:
+                        prod_code = "_".join((fcst_fmt, "obs"))
+                    elif fcst:
+                        prod_code = "_".join((fcst_fmt, mem))
+                    else:
+                        raise Exception
                     for validmin in fcms:
                         validutc = initutc+datetime.timedelta(seconds=60*int(validmin))
                         path_to_pickle = get_extraction_fpaths(vrbl=fcst_vrbl,
